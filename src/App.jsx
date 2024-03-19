0ba09a9f7fc23cc2597244b49934273ac58a26e2
Shikhar Mall <shmall.21.2020@gmail.com>
2024-03-19 22:51:22 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+import ChatPage from "./pages/ChatPage";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
+  }, [location.pathname]); // triggered on route change
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <Routes>
+      <Route exact path="/" element={<ChatPage />} />
+    </Routes>
   );
 }
 
