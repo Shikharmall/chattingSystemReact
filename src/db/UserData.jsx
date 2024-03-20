385446092c04ca81963cadbab3b16cbac15a494d
Shikhar Mall <shmall.21.2020@gmail.com>
2024-03-20 13:39:44 +0530

    added image link

--- src/db/UserData.jsx
+++ src/db/UserData.jsx
@@ -2,47 +2,47 @@ const userList = [
   {
     id: 1,
     name: "John Doe",
-    image: "/assests/image/user4.png",
+    image: "https://robohash.org/Terry.png?set=set4",
   },
   {
     id: 2,
     name: "Jane Smith",
-    image: "/assests/image/user2.png",
+    image: "https://robohash.org/Sheldon.png?set=set4",
   },
   {
     id: 3,
     name: "Michael Johnson",
-    image: "/assests/image/user3.png",
+    image: "https://robohash.org/Terrill.png?set=set4",
   },
   {
     id: 4,
     name: "Emily Williams",
-    image: "/assests/image/user1.png",
+    image: "https://robohash.org/Miles.png?set=set4",
   },
   {
     id: 5,
     name: "David Brown",
-    image: "/assests/image/user5.png",
+    image: "https://robohash.org/Mavis.png?set=set4",
   },
   {
     id: 6,
     name: "Sarah Davis",
-    image: "/assests/image/user7.png",
+    image: "https://robohash.org/Alison.png?set=set4",
   },
   {
     id: 7,
     name: "Matthew Wilson",
-    image: "/assests/image/user6.png",
+    image: "https://robohash.org/Oleta.png?set=set4",
   },
   {
     id: 8,
     name: "Olivia Taylor",
-    image: "/assests/image/user8.png",
+    image: "https://robohash.org/Ewell.png?set=set4",
   },
   {
     id: 9,
     name: "Christopher Martinez",
-    image: "/assests/image/user9.png",
+    image: "https://robohash.org/Demetrius.png?set=set4",
   },
 ];
 export default userList;
